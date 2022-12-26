f37ea194cfd1bee373d29edf4243114339434e98
Ozi <oguzkaanyildiz@mac-ozi.local>
2022-12-26 20:28:53 +0100

    Add cross-field validation for first and last name

--- javagram/src/main/java/ozi/javagram/JavagramController.java
+++ javagram/src/main/java/ozi/javagram/JavagramController.java
@@ -24,6 +24,8 @@ public class JavagramController {
 
     @PostMapping("/submitItem")
     public String handleSubmit(@Valid User user, BindingResult result) {
+        if (user.getFirstName().equals(user.getLastName()))
+            result.rejectValue("lastName", "", "Please enter valid data");
         if (result.hasErrors())
             return "sign-up";
         return "redirect:/result";
